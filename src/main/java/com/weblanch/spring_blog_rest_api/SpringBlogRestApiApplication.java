924c66101fdb06cde3c1019547a02ea9c0961934
s-mansuri <smansuri960@gmail.com>
2024-10-02 14:33:50 +0530

    pushed all code

---
+++ src/main/java/com/weblanch/spring_blog_rest_api/SpringBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.weblanch.spring_blog_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogRestApiApplication.class, args);
+	}
+
+}
